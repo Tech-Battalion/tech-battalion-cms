56139a85f7da31f230707aa98f96df919d556dc3
markdavidmc0 <mark@dotmodus.com>
2020-05-24 10:36:16 +0200

    Initialise tech_news blog.

--- tech_battalion_cms/tech_battalion_cms/settings/base.py
+++ tech_battalion_cms/tech_battalion_cms/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tech_news',
     'wagtail.api.v2',
     'rest_framework'
 ]
